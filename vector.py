0427f2e6cae850e1ac9e786014a4e28685a6e218
nice <2024ucp1367@mnit.ac.in>
2025-11-13 12:50:06 +0530

    changes made in vector file by harshil

--- vector.py
+++ vector.py
@@ -1,12 +1,11 @@
 # This is a python program
 from concurrent.futures import ThreadPoolExecutor
-
+"""nice know nothing about python"""
 def vector_add(a, b):
     return [x + y for x, y in zip(a, b)]
 
 def dot_product(a, b):
 
-    """Calculates the dot product of two vectors."""
     if len(a) != len(b):
         raise ValueError("Vectors must be of the same length.")
     return sum([x * y for x, y in zip(a, b)])
@@ -20,5 +19,5 @@ if __name__ == "__main__":
     v1 = [1, 2, 3]
     v2 = [4, 5, 6]
     v3 = [10,11,12]
-    print("Vector Addition:", vector_add(v1, v2))
-    print("Parallel Dot Product:", dot_product(v1, v2))
+    print("Vector sum:", vector_add(v1, v2))
+    print("Parallel Dot Product of vector:", dot_product(v1, v2))
